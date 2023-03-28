67db367269377555d7c18e55eef91c57800dc876
miggyMedina <127177620+miggyMedina@users.noreply.github.com>
2023-03-28 16:55:06 +0800

    newly updated

--- src/App.js
+++ src/App.js
@@ -5,38 +5,34 @@ import { useState } from 'react';
 export default function App() {
   const [age, setAge] = useState('');
 
-  const handleAge = (e) => {
-    setAge(e.target.value);
-    
-  };
+
+
+
 
   const handleSubmit = (e) => {
-  const inputAge  = e.target.age.value
+    const inputAge = e.target.age.value;
     if (inputAge >= 18) {
-    
-      setAge(`you are ${inputAge} , the right age ` )
+      setAge(`you are ${inputAge} , the right age `);
+    } else if (inputAge == "") {
+      setAge('you did not input anything , please try again');
     } else {
-      setAge(`you are just ${inputAge}, you are underage!`)
+      setAge(`you are just ${inputAge}, you are underage!`);
     }
     e.preventDefault();
   };
 
   return (
-    <div className="container">
+    <div>
       <div className="form-container">
-        <form onSubmit = {handleSubmit}>
-        <div>
+        <form onSubmit={handleSubmit}>
+          <h1>Join our club now</h1>
           <label>Age: </label>
-          <input
-            type="text"
-            className="age-input"
-            name="age"
-          />
-        </div>
-        <input type="submit" className="submit-btn" value="check"  />
-        <h2>{age}</h2>
+          <input type="text" className="age-input" name="age" />
+
+          <input type="submit" className="submit-btn" value="check" />
         </form>
       </div>
+      <h2 >{age}</h2>
     </div>
   );
 }
